564fbda3fc9935f51c46b8ed849ef5f699796d09
Lawrence Shea <webwizardma@gmail.com>
2018-05-14 13:26:36 -0400

    Update post.php

--- post.php
+++ post.php
@@ -15,23 +15,28 @@ $gump->filter_rules([
     'message' => 'trim|sanitize_string'
 ]);
 $data = $gump->run($data);
-$data = [
-    "name"      => $data["name"],
-    "email"     => "From:".($data["email"]),
-    "message"   => $data["message"],
-    "photo"     => $_FILES["photo"] ?? null,
-    "deliver"   => "youremail@email.com",
-    "subject"   => "New Message",
-    "filePath"  => $_FILES["photo"] ? getcwd()."/".$_FILES["photo"]["name"] : null,
-];
-$data["body"] = "From: {$data["name"]} \r\n ImageName: {$data["photo"]["name"]} \r\n Message: {$data["message"]}";
-
-if (!$data["email"]) {
-    echo "You must submit an email address!";
-} elseif (!$data["filePath"] || !move_uploaded_file($data["photo"]["tmp_name"], $data["filePath"])) {
-    echo "Upload failed!";
-} elseif (!mail($data["deliver"], $data["subject"], $data["body"], $data["email"])) {
-    echo "Failed to send!";
+if($data === false) {
+	echo $gump->get_readable_errors(true);
 } else {
+
+  $data = [
+      "name"      => $data["name"],
+      "email"     => "From:".($data["email"]),
+      "message"   => $data["message"],
+      "photo"     => $_FILES["photo"] ?? null,
+      "deliver"   => "youremail@email.com",
+      "subject"   => "New Message",
+      "filePath"  => $_FILES["photo"] ? getcwd()."/".$_FILES["photo"]["name"] : null,
+  ];
+  $data["body"] = "From: {$data["name"]} \r\n ImageName: {$data["photo"]["name"]} \r\n Message: {$data["message"]}";
+
+  if (!$data["email"]) {
+    echo "You must submit an email address!";
+  } elseif (!$data["filePath"] || !move_uploaded_file($data["photo"]["tmp_name"], $data["filePath"])) {
+    echo "Upload failed!";
+  } elseif (!mail($data["deliver"], $data["subject"], $data["body"], $data["email"])) {
+    echo "Failed to send!";
+  } else {
     echo "Email sent!";
+  }
 }
